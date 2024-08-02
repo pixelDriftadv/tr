60478d9528abdcbd4cf49512734e71f02ef273be
pixelDriftadv <pixeldrift.adv@proton.me>
2024-08-03 03:13:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen bg-[#7289da] flex-col space-y-6 py-24 px-6">
-      <h1 className=" text-white font-extrabold text-5xl">Now makhdoom? <br/>just be abandoned forever</h1>
+      <h1 className=" text-white font-extrabold text-5xl">Now makhdoom? <br/>just be abandoned forever <br/> byebye</h1>
     </main>
   );
 }
