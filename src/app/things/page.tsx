31e6cf5a0a81a603415ff8663924fd298eb279e4
pixelDriftadv <pixeldrift.adv@proton.me>
2024-08-02 10:39:39 +0530

    Update page.tsx

--- src/app/things/page.tsx
+++ src/app/things/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Page = () => {
   return (
     <main className="flex min-h-screen bg-[#7289da] flex-col items-center text-center space-y-6 p-24">
-      <h1 className="text-white font-semibold text-xl">Besides could you say who is itz_sahil_732 and itz_kalam afterall? <br/>
+      <h1 className="text-white font-semibold text-xl">Besides could you say who is itz_sahil_732, asiff_zayn and itz_kalam afterall? <br/>
       if I get to know {"you're"} lying or catch you lying then dont expect me either, I got myself to a place I could absolutely abandon you, so be honest</h1>
       <h1 className="text-white font-semibold text-xl">Also, why have you kept blocking me all the time while its easy for you to follow other boys? And you could also laugh with other boys in my absence, but not with me<br/>
        Look, If you dont like me just say it already, I request you to not waste my time or yours, its completely useless, <br/>
