4dda783e084a7854943fb3f6c9ffb73f9954b21d
pixelDriftadv <pixeldrift.adv@proton.me>
2024-08-04 00:48:11 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-[#7289da] flex-col space-y-6 py-24 px-6">
-      <h1 className=" text-white font-extrabold text-5xl">Now makhdoom? <br/>just be abandoned forever <br/> byebye</h1>
+    <main className="flex min-h-screen bg-black flex-col space-y-6 py-24 px-6">
+      <h1 className=" text-red-600 font-extrabold text-5xl">{"I'm either gonna kill myself or just erase all of your memories"}</h1>
+      <h1 className=" text-white/80 font-semibold text-xl">{'I should have just blocked when you laughed on "tera baap ka website ka hai" by that little bonehead bastard'} <br/>
+        Mom-promise I will never interact you ever again in my fucking life
+      </h1>
     </main>
   );
 }
