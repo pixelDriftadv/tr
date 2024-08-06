a646bd0adff7e050397085ff2e9c1678ef32484d
pixelDriftadv <pixeldrift.adv@proton.me>
2024-08-06 07:55:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://jumpshare.com/s/cEoho7UQsVGWHglBxlfX',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
